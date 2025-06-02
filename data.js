e4ca845824c7d7d4be7eeb9fa30808d183b22abb
NBaiel81 <nurmatbekov0771503907@gmail.com>
2025-06-02 06:48:18 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,359 @@
+export const data = {
+    "games": [
+        {
+            "id": "game0",
+            "name": "Strategy Game for PC",
+            "isSale": true,
+            "isBundle": false,
+            "price": 30,
+            "discount": 50,
+            "discountTime": "2025-06-05T23:59:59",
+            "genre": "Strategy",
+            "platform": "PC",
+            "wishlist": true,
+            "bestSelling": true,
+            "rating": 4.5,
+            "images": ["img1.jpg", "img2.jpg"],
+            "description": "A turn-based strategy game set in medieval times.",
+            "systemRequirements": "Windows 10, i5 CPU, 8GB RAM, GTX 1060",
+            "timer": true,
+            "communityReview": "Very engaging with challenging mechanics.",
+            "userReviews": [
+                {"user": "User1", "stars": 5, "text": "Awesome game!"},
+                {"user": "User2", "stars": 4, "text": "Good mechanics."}
+            ],
+            "steamReview": 92,
+            "isNew": true
+        },
+        {
+            "id": "game1",
+            "name": "Puzzle Adventure Game for Switch",
+            "isSale": true,
+            "isBundle": false,
+            "price": 25,
+            "discount": 30,
+            "discountTime": "2025-06-05T09:21:19.278870",
+            "genre": "Puzzle",
+            "platform": "Switch",
+            "wishlist": false,
+            "bestSelling": false,
+            "rating": 4.2,
+            "images": ["Puzzle_Adventure_Game_for_Switch_1.jpg", "Puzzle_Adventure_Game_for_Switch_2.jpg"],
+            "description": "Solve intricate puzzles across an enchanting world.",
+            "systemRequirements": "Nintendo Switch Console",
+            "timer": true,
+            "communityReview": "Challenging and fun for all ages.",
+            "userReviews": [
+                {"user": "PuzzleLover", "stars": 5, "text": "Absolutely brilliant puzzles."},
+                {"user": "Thinker82", "stars": 4, "text": "Great game with some tricky moments."}
+            ],
+            "steamReview": 88,
+            "isNew": true
+        },
+        {
+            "id": "game2",
+            "name": "Sci-Fi RPG Game for Xbox",
+            "isSale": false,
+            "isBundle": false,
+            "price": 35,
+            "discount": 0,
+            "discountTime": null,
+            "genre": "RPG",
+            "platform": "Xbox",
+            "wishlist": true,
+            "bestSelling": true,
+            "rating": 4.7,
+            "images": ["Sci_Fi_RPG_Game_for_Xbox_1.jpg", "Sci_Fi_RPG_Game_for_Xbox_2.jpg"],
+            "description": "Explore galaxies and make choices that shape your destiny.",
+            "systemRequirements": "Xbox Series X/S",
+            "timer": false,
+            "communityReview": "Immersive and visually stunning.",
+            "userReviews": [
+                {"user": "SciFiFan99", "stars": 5, "text": "Incredible story and visuals!"},
+                {"user": "RPGMaster", "stars": 4, "text": "Loved the branching storylines."}
+            ],
+            "steamReview": 91,
+            "isNew": false
+        },
+        {
+            "id": "game3",
+            "name": "Casual Puzzle Game for Mobile",
+            "isSale": true,
+            "isBundle": false,
+            "price": 10,
+            "discount": 20,
+            "discountTime": "2025-06-04T09:26:11.461536",
+            "genre": "Puzzle",
+            "platform": "Mobile",
+            "wishlist": false,
+            "bestSelling": false,
+            "rating": 3.8,
+            "images": ["Casual_Puzzle_Game_for_Mobile_1.jpg", "Casual_Puzzle_Game_for_Mobile_2.jpg"],
+            "description": "Relaxing mobile game with colorful puzzles and daily challenges.",
+            "systemRequirements": "iOS/Android, 2GB RAM",
+            "timer": true,
+            "communityReview": "Fun to play during breaks.",
+            "userReviews": [
+                {"user": "CasualGamer", "stars": 4, "text": "Perfect for killing time!"},
+                {"user": "PuzzleFan", "stars": 3, "text": "Wish there were more levels."}
+            ],
+            "steamReview": 76,
+            "isNew": true
+        },
+        {
+            "id": "game4",
+            "name": "Educational Simulation Game for Web",
+            "isSale": false,
+            "isBundle": false,
+            "price": 15,
+            "discount": 0,
+            "discountTime": null,
+            "genre": "Simulation",
+            "platform": "Web",
+            "wishlist": true,
+            "bestSelling": false,
+            "rating": 4.0,
+            "images": ["Educational_Simulation_Game_for_Web_1.jpg", "Educational_Simulation_Game_for_Web_2.jpg"],
+            "description": "Learn economics and resource management through this interactive simulation game.",
+            "systemRequirements": "Modern web browser, 4GB RAM",
+            "timer": false,
+            "communityReview": "A smart way to learn while playing.",
+            "userReviews": [
+                {"user": "TeacherX", "stars": 4, "text": "Great for students!"},
+                {"user": "SimFan", "stars": 4, "text": "Well designed and educational."}
+            ],
+            "steamReview": 84,
+            "isNew": true
+        },
+        {
+            "id": "game5",
+            "name": "Rhythm Music Game for PS",
+            "isSale": true,
+            "isBundle": false,
+            "price": 20,
+            "discount": 25,
+            "discountTime": "2025-06-06T09:28:50.949012",
+            "genre": "Rhythm",
+            "platform": "PS",
+            "wishlist": false,
+            "bestSelling": true,
+            "rating": 4.6,
+            "images": ["Rhythm_Music_Game_for_PS_1.jpg", "Rhythm_Music_Game_for_PS_2.jpg"],
+            "description": "Tap to the beat and master over 100 music tracks in this energetic rhythm game.",
+            "systemRequirements": "PlayStation 4/5",
+            "timer": true,
+            "communityReview": "Addictive gameplay with an amazing soundtrack.",
+            "userReviews": [
+                {"user": "BeatMaster", "stars": 5, "text": "Music and gameplay are top-notch!"},
+                {"user": "DanceFan22", "stars": 4, "text": "Very fun, some hard levels."}
+            ],
+            "steamReview": 89,
+            "isNew": false
+        },
+        {
+            "id": "game6",
+            "name": "Local Co-op Party Game for PC",
+            "isSale": false,
+            "isBundle": false,
+            "price": 18,
+            "discount": 0,
+            "discountTime": null,
+            "genre": "Party",
+            "platform": "PC",
+            "wishlist": true,
+            "bestSelling": false,
+            "rating": 4.1,
+            "images": ["Local_Coop_Party_Game_for_PC_1.jpg", "Local_Coop_Party_Game_for_PC_2.jpg"],
+            "description": "Gather your friends for mini-game chaos in this hilarious party title.",
+            "systemRequirements": "Windows 10, 4GB RAM, integrated graphics",
+            "timer": false,
+            "communityReview": "Great for game nights with friends!",
+            "userReviews": [
+                {"user": "PartyPlayer", "stars": 4, "text": "Laughs guaranteed."},
+                {"user": "FriendZone", "stars": 4, "text": "Needs more minigames but fun!"}
+            ],
+            "steamReview": 83,
+            "isNew": true
+        },
+        {
+            "id": "game7",
+            "name": "Horror Survival Game for PS",
+            "isSale": true,
+            "isBundle": false,
+            "price": 40,
+            "discount": 40,
+            "discountTime": "2025-06-08T12:00:00",
+            "genre": "Horror",
+            "platform": "PS",
+            "wishlist": false,
+            "bestSelling": true,
+            "rating": 4.9,
+            "images": ["Horror_Survival_Game_for_PS_1.jpg", "Horror_Survival_Game_for_PS_2.jpg"],
+            "description": "Survive the night in a haunted mansion filled with terrifying creatures.",
+            "systemRequirements": "PlayStation 4/5, 8GB RAM",
+            "timer": true,
+            "communityReview": "Terrifying atmosphere and great story.",
+            "userReviews": [
+                {"user": "ScaryCat", "stars": 5, "text": "Best horror game I've played!"},
+                {"user": "NightOwl", "stars": 4, "text": "Creepy and immersive."}
+            ],
+            "steamReview": 95,
+            "isNew": false
+        },
+        {
+            "id": "game8",
+            "name": "Fantasy Action Game for Xbox",
+            "isSale": true,
+            "isBundle": false,
+            "price": 50,
+            "discount": 20,
+            "discountTime": "2025-06-09T15:30:00",
+            "genre": "Action",
+            "platform": "Xbox",
+            "wishlist": true,
+            "bestSelling": true,
+            "rating": 4.8,
+            "images": ["Fantasy_Action_Game_for_Xbox_1.jpg", "Fantasy_Action_Game_for_Xbox_2.jpg"],
+            "description": "Embark on an epic quest in a magical world filled with mythical creatures.",
+            "systemRequirements": "Xbox Series X/S, 12GB RAM",
+            "timer": true,
+            "communityReview": "Epic battles and stunning graphics.",
+            "userReviews": [
+                {"user": "EpicGamer", "stars": 5, "text": "Loved the combat system!"},
+                {"user": "QuestLover", "stars": 4, "text": "Great story and characters."}
+            ],
+            "steamReview": 90,
+            "isNew": false
+        },
+        {
+            "id": "game9",
+            "name": "Indie Platformer for PC",
+            "isSale": false,
+            "isBundle": false,
+            "price": 15,
+            "discount": 0,
+            "discountTime": null,
+            "genre": "Platformer",
+            "platform": "PC",
+            "wishlist": true,
+            "bestSelling": false,
+            "rating": 4.3,
+            "images": ["Indie_Platformer_for_PC_1.jpg", "Indie_Platformer_for_PC_2.jpg"],
+            "description": "A charming indie platformer with hand-drawn art and challenging levels.",
+            "systemRequirements": "Windows 10, 4GB RAM, integrated graphics",
+            "timer": false,
+            "communityReview": "Beautiful art style and fun gameplay.",
+            "userReviews": [
+                {"user": "ArtLover", "stars": 5, "text": "Stunning visuals!"},
+                {"user": "PlatformerFan", "stars": 4, "text": "Challenging but rewarding."}
+            ],
+            "steamReview": 87,
+            "isNew": true
+        },
+        {
+            "id": "game10",
+            "name": "Multiplayer Shooter for PS",
+            "isSale": true,
+            "isBundle": false,
+            "price": 45,
+            "discount": 30,
+            "discountTime": "2025-06-10T18:00:00",
+            "genre": "Shooter",
+            "platform": "PS",
+            "wishlist": false,
+            "bestSelling": true,
+            "rating": 4.4,
+            "images": ["Multiplayer_Shooter_for_PS_1.jpg", "Multiplayer_Shooter_for_PS_2.jpg"],
+            "description": "Join the battle in this fast-paced multiplayer shooter with various game modes.",
+            "systemRequirements": "PlayStation 4/5, 8GB RAM",
+            "timer": true,
+            "communityReview": "Intense and addictive gameplay.",
+            "userReviews": [
+                {"user": "ShooterFan", "stars": 5, "text": "Best shooter I've played!"},
+                {"user": "TeamPlayer", "stars": 4, "text": "Great teamwork mechanics."}
+            ],
+            "steamReview": 91,
+            "isNew": false
+        }
+    ],
+    "bundles": [
+        {
+            "id": "bundle1",
+            "name": "Strategy Pack",
+            "isBundle": true,
+            "platform": "PC",
+            "wishlist": false,
+            "rating": 4.8,
+            "description": "A collection of strategy games for PC enthusiasts.",
+            "images": ["bundle1.jpg"],
+            "basePrice": 30, // Use the highest tier price as the base
+            "tiers": [
+                { "price": 10, "gamesIncluded": ["Strategy Game A", "Tactics Game B"] },
+                { "price": 20, "gamesIncluded": ["Strategy Game A", "Tactics Game B", "War Game C"] },
+                { "price": 30, "gamesIncluded": ["Strategy Game A", "Tactics Game B", "War Game C", "Empire Game D", "Conquest Game E"] }
+            ],
+            "discount": 50,
+            "discountTime": "2025-06-07T23:59:59",
+            "timer": true,
+            "communityReview": "Amazing value for strategy lovers.",
+            "userReviews": [
+                { "user": "BundleFan1", "stars": 5, "text": "Best bundle I've bought." },
+                { "user": "BudgetGamer", "stars": 4, "text": "Saved a lot." }
+            ],
+            "steamReview": 89,
+            "isNew": false
+        },
+        {
+            "id": "bundle2",
+            "name": "Horror Collection",
+            "isBundle": true,
+            "platform": "PS",
+            "wishlist": false,
+            "rating": 4.7,
+            "description": "Terrifying horror games for PS players.",
+            "images": ["bundle2.jpg"],
+            "basePrice": 35, // Use the highest tier price as the base
+            "tiers": [
+                { "price": 15, "gamesIncluded": ["Horror Game A", "Scary Game B"] },
+                { "price": 25, "gamesIncluded": ["Horror Game A", "Scary Game B", "Nightmare Game C"] },
+                { "price": 35, "gamesIncluded": ["Horror Game A", "Scary Game B", "Nightmare Game C", "Terror Game D"] }
+            ],
+            "discount": 40,
+            "discountTime": "2025-06-08T23:59:59",
+            "timer": true,
+            "communityReview": "Perfect for horror fans.",
+            "userReviews": [
+                { "user": "HorrorFan", "stars": 5, "text": "So creepy!" },
+                { "user": "ThrillSeeker", "stars": 4, "text": "Great scares." }
+            ],
+            "steamReview": 92,
+            "isNew": false
+        },
+        {
+            "id": "bundle3",
+            "name": "Indie Gems",
+            "isBundle": true,
+            "platform": "PC",
+            "wishlist": false,
+            "rating": 4.5,
+            "description": "A curated selection of indie games for PC.",
+            "images": ["bundle3.jpg"],
+            "basePrice": 25, // Use the highest tier price as the base
+            "tiers": [
+                { "price": 5, "gamesIncluded": ["Indie Game A"] },
+                { "price": 15, "gamesIncluded": ["Indie Game A", "Indie Game B", "Indie Game C"] },
+                { "price": 25, "gamesIncluded": ["Indie Game A", "Indie Game B", "Indie Game C", "Indie Game D", "Indie Game E"] }
+            ],
+            "discount": 60,
+            "discountTime": "2025-06-09T23:59:59",
+            "timer": true,
+            "communityReview": "Hidden gems worth every penny.",
+            "userReviews": [
+                { "user": "IndieLover", "stars": 5, "text": "Amazing indie titles!" },
+                { "user": "GamerX", "stars": 4, "text": "Really fun games." }
+            ],
+            "steamReview": 87,
+            "isNew": true
+        }
+    ]
+};
